e3001b8c2d466840699262f9e15677a2f58b96d0
wbluke <pwb0127@gmail.com>
2021-01-28 20:47:11 +0900

    feat: Movie list fetching with css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -6,18 +9,39 @@ class App extends React.Component {
     movies: []
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 6000);
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
+  }
+
+  async componentDidMount() {
+    this.getMovies();
   }
 
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : "We are ready"}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader-text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
